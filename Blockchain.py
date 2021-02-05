a97c570949e1fb5eea6418cdd784fb1756c6bc52
Michael Mandulak <53231924+mmandulak1@users.noreply.github.com>
2021-02-05 10:12:16 -0500

    Debugged code and added mining function

--- Blockchain.py
+++ Blockchain.py
@@ -5,11 +5,12 @@ from flask import Flask, request
 
 
 class Block_info: # holds all the information for each block
-    def __init__(self, index, status, timestamp, prev_hash):
+    def __init__(self, index, transaction, timestamp, prev_hash, nonce=0):
         self.index = index  # index of block
-        self. status = status  # status ex. pending or accepted
+        self.transaction = transaction  # status ex. pending or accepted
         self.timestamp = timestamp  # timestamp of when created
         self.prev_hash = prev_hash  # holds hash of previous block in chain
+        self.nonce = nonce
 
     def hash_func(self):  # hash function to sha 256
         return sha256(json.dumps(self.__dict__, sort_keys=True).encode()).hexdigest()
@@ -42,15 +43,17 @@ class Blockchain:
 
     difficulty = 2
 
-    def testing(self, info):
-        hash = Block_info.hash_func()
+    def testing(self, block):
+        hash = block.hash_func()
+        block.nonce = 0
         while not hash.startswith('0' * Blockchain.difficulty):
-            hash = Block_info.hash_func()
-            return hash
+            block.nonce += 1
+            hash = block.hash_func()
+        return hash
 
     def add_block(self, block, proof):
         prev_hash = self.last_block.hash
-        if prev_hash != block.previous_hash:
+        if prev_hash != block.prev_hash:
             return False
         if not self.is_valid_proof(block, proof):
             return False
@@ -60,35 +63,43 @@ class Blockchain:
 
     def is_valid_proof(self, block, block_hash):
         return (block_hash.startswith('0' * Blockchain.difficulty) and
-                block_hash == block.compute_hash())
+                block_hash == block.hash_func())
 
     def add_new_transaction(self, transaction):
-        self.unconfirmed_transactions.append(transaction)
+        self.pend_queue.append(transaction)
 
     def mine(self):
-        if not self.unconfirmed_transactions:
+        if not self.pend_queue:
             return False
 
         last_block = self.last_block
 
         new_block = Block_info(index=last_block.index + 1,
-                          transactions=self.unconfirmed_transactions,
-                          timestamp=time.time(),
-                          previous_hash=last_block.hash)
+                               transaction=self.pend_queue,
+                               timestamp=time.time(),
+                               prev_hash=last_block.hash)
 
-        proof = self.proof_of_work(new_block)
+        proof = self.testing(new_block)
         self.add_block(new_block, proof)
-        self.unconfirmed_transactions = []
+        self.pend_queue= []
         return new_block.index
 
 app =  Flask(__name__)
 blockchain = Blockchain()
 
+
+# noinspection PyInterpreter,PyInterpreter
 @app.route('/chain', methods=['GET'])
 def get_chain():
     chain_data = []
+    blockchain.mine()
     for block in blockchain.chain:
         chain_data.append(block.__dict__)
     return json.dumps({"length": len(chain_data),
                        "chain": chain_data})
-app.run(debug=True, port=5000)
+@app.route('/mine', methods=['GET'])
+def miner():
+    blockchain.add_new_transaction("data")
+    blockchain.mine()
+    return json.dumps({"success": True})
+app.run(debug=True, port=8080)
